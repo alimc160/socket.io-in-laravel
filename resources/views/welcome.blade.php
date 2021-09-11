44cb959d59ce05b54fbad09c03d8d34a23c4b4d3
aliazmattechnerds <muhammadazmat@technerdslab.com>
2021-09-11 06:42:06 -0700

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        </style>
+        <body>
+           <script src="https://cdn.socket.io/4.1.2/socket.io.min.js" integrity="sha384-toS6mmwu70G0fw54EGlWWeA4z3dyJ+dlXBtSURSKN4vyRFOcxd3Bzjj/AoOwY+Rg" crossorigin="anonymous"></script>
+           <script>
+                const ip_address = '127.0.0.1';
+                const socket_port = '3000';
+                let socket = io(ip_address + ':' + socket_port);
+                socket.on('connection');
+           </script>
+        </body>
+    </head>
+</html>
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+
+const  port = 3000;
+
+const io = require('socket.io')(server,{
+   cors:{ origin : "*" }
+});
+
+io.on('connection',(socket)=>{
+    console.log('connected');
+    socket.on('disconnect',(socket)=>{
+        console.log('disconnect');
+    });
+})
+server.listen(port, ()=>{
+    console.log(`server running at http://localhost:${port}`);
+});
